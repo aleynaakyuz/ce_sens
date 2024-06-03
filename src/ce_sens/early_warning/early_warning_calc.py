501f2e40beeca4b9ee802fe29da60963b259e06b
Aleyna Akyuz <aleyna@Aleyna-MacBook-Pro-5.local>
2024-06-02 22:45:43 -0400

    adding range to early warning

--- src/ce_sens/early_warning/early_warning_calc.py
+++ src/ce_sens/early_warning/early_warning_calc.py
@@ -15,6 +15,8 @@ def early_warning_calc():
     parser.add_argument("time", nargs=1, type=int, help="Early warning time in seconds. If there are more than one time requested, give as a list")
     parser.add_argument("snr_path_1", type=str, help="First detector that has snr calculation")
     parser.add_argument("snr_path_2", help="First detector that has snr calculation")
+    parser.add_argument("end", type=int, help="end of the parameter index")
+    parser.add_argument("det", help="detector names")
     parser.add_argument("dynamic_psd", nargs='?', help="First detector that has snr calculation")
     parser.add_argument("lag", type=float, nargs='?', help="If the psd is dynamic, switch time before the intersection")
     parser.add_argument("switch_duration", type=float, nargs='?', help="If psd is dynamic, duration of the switch")
@@ -38,6 +40,9 @@ def early_warning_calc():
     psd_path_2 = args.psd_path_2
     psd_path_3 = args.psd_path_3
     dynamic_psd = args.dynamic_psd
+
+    start = args.start 
+    end = args.end
 
     low_freq_dic = {'J1': 5.2, 'J2':5.2, 'E1':1, 'I1': 3}
     df = 0.1 ## REVISIT THIS
@@ -72,8 +77,8 @@ def early_warning_calc():
         param_list = get_parameter_list(param_path, apx, df, low_freq_dic[det])
         detected_params = np.array(param_list)[detections]
         for t in time:
-            for param in tqdm(detected_params):
-                ew_snrs = early_warning(t, param, det, psd_dic[det], dynamic_psd, lag, switch_duration)
+            for i in range(start,end):
+                ew_snrs = early_warning(t, detected_params[i], det, psd_dic[det], dynamic_psd, lag, switch_duration)
             hf.create_dataset(str(t), data=ew_snrs)
         hf.create_dataset(str(det), data=snr_l)
     hf.close()
