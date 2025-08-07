4e303a82ee17ec617437f3e98fcc65e76bda179c
Aleyna Akyuz <aakyuz@Aleynas-MacBook-Air.local>
2025-08-07 15:36:42 -0400

    update BBH pm

--- src/ce_sens/post_merger/bbh_pm.py
+++ src/ce_sens/post_merger/bbh_pm.py
@@ -30,54 +30,20 @@ def bbh_pm_calc():
     end = args.end
     start = args.start
 
-    time = 1697205750
     slen = 10
     srate = 1000
     tlen = slen * srate
     flen = tlen // 2 + 1
     df = 1.0 / slen
     dt = 1.0 / srate
-    flow = 50
 
     parameters2 = {
     "approximant": "IMRPhenomXPHM",
     "delta_t": dt,
-    "f_lower": flow,
+    "f_lower": 5.1,
     "phase_order": -1
     }
 
-    parameters3 = {
-    "approximant": "IMRPhenomXPHM",
-    "delta_f": 0.01,
-    "f_lower": 5.2,
-    "phase_order": -1
-    }
-    #snr_path_lst = [snr_path_1, snr_path_2, snr_path_3]
-    #snr_paths = [x for x in snr_path_lst if x is not None]
-
-    #psd_path_lst = [psd_path_1, psd_path_2, psd_path_3]
-    #psd_paths = [p for p in psd_path_lst if p is not None]
-
-    #snr_vals_dic = {}
-    #for path in snr_paths:
-    #    snr = h5py.File(path, 'r')
-    #    network_key = list(snr.keys())
-    #    snr_vals_dic[network_key[0]] = snr[network_key[0]][:]
-
-    #det_list = list(snr_vals_dic.keys())
-
-    #psd_dic = {key: psd for key in det_list for psd in psd_paths}
-    #psds = {key:from_txt(psd_dic[key], length=flen, delta_f=df, low_freq_cutoff=flow) for key in psd_dic}
-
-    #net_snr_shape =  len(snr_vals_dic[det_list[0]])
-    #net_snr = np.zeros(net_snr_shape)
-    #for det_snr in snr_vals_dic.values():
-    #    sum_snr = net_snr + det_snr**2
-    #det_snr = np.sqrt(sum_snr)
-
-    #detections = det_snr[start:end] > 10
-
-    #lenn_det = sum(detections)
     det = Detector(args.det)
 
     data_dic = get_dic(input_path)
@@ -86,6 +52,7 @@ def bbh_pm_calc():
     ra = data_dic['ra']
     dec = data_dic['dec']
     pol = data_dic['polarization']
+    tc = data_dic['tc']
 
     psd = from_txt(psd_path, length=int(4000/0.01), delta_f=0.01, low_freq_cutoff=5.2)
 
@@ -93,23 +60,12 @@ def bbh_pm_calc():
     hf = h5py.File(out_path, 'w')
     for i in tqdm(range(start, end)):
         temp_data = {key: value[i] for key, value in data_dic.items()}
-        param = {**temp_data, **parameters3}
+        param = {**temp_data, **parameters2}
         snr = calculate_snr(args.det, psd, param, 5.2)
 
         if snr > 10:
-            param = {**temp_data, **parameters2}
-            try:
-                hp, hc = get_td_waveform(**param)
-            except Exception as e:
-                print(f"Error with sample {i}: {e}. Retrying with updated f_lower...")
-                param['f_lower'] = 40
-                try:
-                    hp, hc = get_td_waveform(**param)
-                except  Exception as e2:
-                    print(f"Retry failed for sample {i}: {e2}. Skipping this sample.")
-                    snr_pm = 0
-                    snr_l.append(snr_pm)
-                    continue
+
+            hp, hc = get_td_waveform(**param)
 
             _, loc_hp = hp.abs_max_loc()
             _, loc_hc = hc.abs_max_loc()
@@ -119,12 +75,12 @@ def bbh_pm_calc():
             hp_pm.resize(lenn)
             hc_pm.resize(lenn)
             df = 1 / lenn
-            hp_pm = hp_pm.to_frequencyseries(delta_f= df * (1 + param['redshift']))
-            hc_pm = hc_pm.to_frequencyseries(delta_f= df * (1 + param['redshift']))
-            fp, fc = det.antenna_pattern(ra[i], dec[i], pol[i], time)
+            hp_pm = hp_pm.to_frequencyseries(delta_f= df)
+            hc_pm = hc_pm.to_frequencyseries(delta_f= df)
+            fp, fc = det.antenna_pattern(ra[i], dec[i], pol[i], tc[i])
             proj_strain = hp_pm * fp + hc_pm * fc
-            psd_pm = from_txt(psd_path, length=len(proj_strain), delta_f= df * (1 + param['redshift']), low_freq_cutoff=flow)
-            snr_pm = sigma(proj_strain, psd=psd_pm, low_frequency_cutoff=flow)
+            psd_pm = from_txt(psd_path, length=len(proj_strain), delta_f= df, low_freq_cutoff=param['f_lower'])
+            snr_pm = sigma(proj_strain, psd=psd_pm, low_frequency_cutoff=param['f_lower'])
 
         else:
             snr_pm = 0
