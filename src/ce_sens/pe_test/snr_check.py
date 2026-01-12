6f98020990fcdd4862a168876573772dfcbe9f31
Aleyna Akyuz <aakyuz@Aleynas-MacBook-Air.local>
2026-01-12 09:10:24 -0500

    adding snr check

---
+++ src/ce_sens/pe_test/snr_check.py
@@ -0,0 +1,38 @@
+import argparse
+import csv
+from pycbc.inference.io import loadfile
+
+def snr_check():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--results")
+    parser.add_argument("--csv-path")
+    parser.add_argument("--injection-snr", type=float)
+
+
+
+    opts = parser.parse_args()
+    path = opts.results
+    csv_path = opts.csv_path
+    injection_snr = opts.injection_snr    
+
+    data = loadfile(path, 'r')
+
+    estimated_snr = (2 * max(data.read_samples(parameters='loglr')['loglr']))**0.5
+
+    difference_snr = abs(estimated_snr - injection_snr) / injection_snr * 100
+    difference_flag = difference_snr > 5.0
+
+
+    row = [
+    path,
+	injection_snr,
+	estimated_snr,
+	difference_snr,
+	difference_flag
+    ]
+
+
+    with open(csv_path, "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(row)    
+    
